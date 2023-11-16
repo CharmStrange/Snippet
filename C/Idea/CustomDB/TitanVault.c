7ddda87a9960a11f4514c29dc8f84224abbfddea
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-11-16 13:07:22 +0900

    Update TitanVault.c

--- C/Idea/CustomDB/TitanVault.c
+++ C/Idea/CustomDB/TitanVault.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define DONE 0
 
 struct structure{
@@ -7,6 +8,15 @@ struct structure{
     double data_double;
 };
 
+void DynamicAllocation(int size){
+    struct structure* s;
+    s = malloc(sizeof(struct structure));
+
+    struct structure* Array = (struct structure*)malloc(sizeof(struct structure) * size);
+    
+}
+
 int main() {
+    DynamicAllocation(5);
    return DONE; 
 }

8126583e33babfd6a97e13a25aeee1e13627f3ae
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-02-20 15:18:08 +0900

    Update LuaContainers.lua

--- Lua/LuaContainers.lua
+++ Lua/LuaContainers.lua
@@ -1,95 +1,25 @@
 -- LuaContainers.lua
 
--- Define the LuaContainers class
 local LuaContainers = {}
 
--- List Class
-LuaContainers.List = {
-  new = function()
-    return {}
-  end,
-  
-  add = function(list, item)
-    table.insert(list, item)
-  end,
-  
-  remove = function(list, index)
-    table.remove(list, index)
-  end,
-  
-  size = function(list)
-    return #list
-  end,
-  
-  get = function(list, index)
-    return list[index]
-  end,
-  
-  clear = function(list)
-    for k in ipairs(list) do
-      list[k] = nil
-    end
-  end,
-  
-  contains = function(list, item)
-    for _, value in ipairs(list) do
-      if value == item then
-        return true
-      end
-    end
-    return false
-  end
-}
+local List = {}
+setmetatable(List, LuaContainers)
+print(getmetatable(List))
 
--- DynamicList Class (inherits from List)
-LuaContainers.DynamicList = LuaContainers.List
-LuaContainers.DynamicList.add = function(list, item)
-  list[#list + 1] = item
-end
+local DynamicList = {}
+setmetatable(DynamicList, LuaContainers)
+print(getmetatable(DynamicList))
 
--- Stack Class (inherits from List)
-LuaContainers.Stack = LuaContainers.List
-LuaContainers.Stack.push = function(stack, item)
-  table.insert(stack, item)
-end
-LuaContainers.Stack.pop = function(stack)
-  return table.remove(stack)
-end
-LuaContainers.Stack.peek = function(stack)
-  return stack[#stack]
-end
+local Stack = {}
+setmetatable(Stack, LuaContainers)
+print(getmetatable(Stack))
 
--- Queue Class (inherits from List)
-LuaContainers.Queue = LuaContainers.List
-LuaContainers.Queue.enqueue = function(queue, item)
-  table.insert(queue, 1, item)
-end
-LuaContainers.Queue.dequeue = function(queue)
-  return table.remove(queue)
-end
-LuaContainers.Queue.peek = function(queue)
-  return queue[#queue]
-end
+local Queue = {}
+setmetatable(Queue, LuaContainers)
+print(getmetatable(Queue))
 
--- Deque Class (inherits from List)
-LuaContainers.Deque = LuaContainers.List
-LuaContainers.Deque.pushFront = function(deque, item)
-  table.insert(deque, 1, item)
-end
-LuaContainers.Deque.popFront = function(deque)
-  return table.remove(deque, 1)
-end
-LuaContainers.Deque.pushBack = function(deque, item)
-  table.insert(deque, item)
-end
-LuaContainers.Deque.popBack = function(deque)
-  return table.remove(deque)
-end
-LuaContainers.Deque.peekFront = function(deque)
-  return deque[1]
-end
-LuaContainers.Deque.peekBack = function(deque)
-  return deque[#deque]
-end
+local Deque = {}
+setmetatable(Deque, LuaContainers)
+print(getmetatable(Deque))
 
-return LuaContainers
+return LuaContainers  -- Returning LuaContainers at the end

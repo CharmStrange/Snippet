385a1a8a3e86cc6ed17e6c0a12b6e0d2696d5702
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-02-21 16:43:09 +0900

    Update LuaContainers.lua

--- Lua/LuaContainers.lua
+++ Lua/LuaContainers.lua
@@ -1,82 +1,122 @@
 -- LuaContainers.lua
 
+-- List implementation
 List = {}
-Stack = {}
-Queue = {}
-Deque = {}
-LuaContainers = {List, Stack, Queue, Deque}
 
 function List:new()
-    local container = setmetatable({}, self)
-
-    function container:INSERT(value)
-        container.insert(value)
-    end
-    
-    function continaer:REMOVE(index)
-        container.remove(container, index)
-    end
-    
-    function container:CLEAR()
-        
-    end
-    
-    return container
-    
+    local list = {elements = {}}
+    setmetatable(list, self)
+    self.__index = self
+    return list
 end
+
+function List:insert(value)
+    table.insert(self.elements, value)
+end
+
+function List:remove(index)
+    table.remove(self.elements, index)
+end
+
+function List:clear()
+    self.elements = {}
+end
+
+-- Stack implementation
+Stack = {}
 
 function Stack:new()
-    local container = setmetatable({}, self)
-    
-    function container:INSERT(value)
-        container.insert(value)
-    end
-    
-    function continaer:REMOVE(index)
-        container.remove(container, index)
-    end
-    
-    function container:CLEAR()
-        
-    end
-    
-    return container
+    local stack = {elements = {}}
+    setmetatable(stack, self)
+    self.__index = self
+    return stack
 end
+
+function Stack:push(value)
+    table.insert(self.elements, value)
+end
+
+function Stack:pop()
+    return table.remove(self.elements)
+end
+
+function Stack:clear()
+    self.elements = {}
+end
+
+-- Queue implementation
+Queue = {}
 
 function Queue:new()
-    local container = setmetatable({}, self)
-    
-    function container:INSERT(value)
-        container.insert(value)
-    end
-    
-    function continaer:REMOVE(index)
-        container.remove(container, index)
-    end
-    
-    function container:CLEAR()
-        
-    end
-    
-    return container
+    local queue = {elements = {}}
+    setmetatable(queue, self)
+    self.__index = self
+    return queue
 end
+
+function Queue:enqueue(value)
+    table.insert(self.elements, value)
+end
+
+function Queue:dequeue()
+    return table.remove(self.elements, 1)
+end
+
+function Queue:clear()
+    self.elements = {}
+end
+
+-- Deque implementation
+Deque = {}
 
 function Deque:new()
-    local container = setmetatable({}, self)
-    
-    function container:INSERT(value)
-        container.insert(value)
-    end
-    
-    function continaer:REMOVE(index)
-        container.remove(container, index)
-    end
-    
-    function container:CLEAR()
-        
-    end
-    
-    return container
+    local deque = {first = 0, last = -1, elements = {}}
+    setmetatable(deque, self)
+    self.__index = self
+    return deque
 end
 
---return LuaContainers  -- Returning LuaContainers at the end
+function Deque:pushFront(value)
+    local first = self.first - 1
+    self.first = first
+    self.elements[first] = value
+end
+
+function Deque:pushBack(value)
+    local last = self.last + 1
+    self.last = last
+    self.elements[last] = value
+end
+
+function Deque:popFront()
+    local first = self.first
+    if first > self.last then
+        return nil
+    end
+    local value = self.elements[first]
+    self.elements[first] = nil -- to allow garbage collection
+    self.first = first + 1
+    return value
+end
+
+function Deque:popBack()
+    local last = self.last
+    if self.first > last then
+        return nil
+    end
+    local value = self.elements[last]
+    self.elements[last] = nil -- to allow garbage collection
+    self.last = last - 1
+    return value
+end
+
+function Deque:clear()
+    self.first = 0
+    self.last = -1
+    self.elements = {}
+end
+
+-- LuaContainers table containing all container types
+LuaContainers = {List, Stack, Queue, Deque}
+
+return LuaContainers  -- Returning LuaContainers at the end
